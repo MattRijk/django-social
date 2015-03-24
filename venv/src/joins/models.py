c2d62b5048eb67162eedc9d7c029ce55b5caa2ae
Matt Rijk <matthewrijk@gmail.com>
2015-03-24 18:59:31 -0400

    made changes to joins app

--- venv/src/joins/models.py
+++ venv/src/joins/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Join(models.Model):
-    email = models.EmailField()
-
+    email = models.EmailField(unique=True)
+    ref_id = models.CharField(max_length=120, default='ABC', unique=True)
+    ip_address = models.CharField(max_length=120, default='ABC')
     # auto_now_add -> when it was added to the database ==> set the time.
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
@@ -14,6 +15,9 @@ class Join(models.Model):
     # name = models.CharField(max_length=100, primary_key=True)
     def __unicode__(self):
         return "%s" %(self.email)
+
+    class Meta:
+        unique_together = ("email", "ref_id",)
 
     # def name(self):
     #     return self.name
